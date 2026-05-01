3e83ab947d249aa6d4942fd09541e9b9f31035b5
= <darrenvincent547@gmail.com>
2026-05-01 13:36:26 +0700

    revisi baru konek supabase

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,5 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 
+console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("KEY:", process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY);
+
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
